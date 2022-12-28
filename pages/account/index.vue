16d1b763661d47ddfbeb1307f1d22100681b0798
mannnakakunn <mannnakakunn@gmail.com>
2022-12-29 07:25:35 +0900

    add auth function beta 2

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,6 +1,19 @@
 <script setup lang="ts">
 // supabaseAuth
 const supabase = useSupabaseClient();
+const query = useRoute();
+const user = useSupabaseUser();
+
+const loading = ref(false);
+
+watchEffect(async () => {
+  if (user.value) {
+    await navigateTo(query.redirectTo as string, {
+      replace: true,
+    });
+  }
+});
+
 const login = async () => {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "github",
@@ -9,45 +22,41 @@ const login = async () => {
     console.error(error);
   }
 };
-
 </script>
 
 <template>
   <Header />
-    <div
-      class="d-flex justify-center align-center"
-      style="height: 80vh; background-color: #e0ece8"
-    >
-      <div class="d-flex flex-column justify-center align-center">
-        <v-img
-          style="width: 30vw; height: 30vw; border-radius: 50%"
-          src="https://picsum.photos/id/400/300/300"
-        />
-        <v-form class="row">
-          <v-row
-            class="d-flex flex-center flex justify-center align-center col-6 form-widget px-4 text-center"
-          >
-            <v-col>
-              <div
-                class="text-h5 text-teal-darken-4 font-weight-bold header pt-6"
-              >
-                Sign in
-              </div>
-              <div class="text-subtitle-3 pt-1 pb-3 text-teal">
-                Sign in with Social media account
-              </div>
-              <div
-                class="d-flex flex-row justify-center align-center logobtn py-2"
-                @click="login"
-              >
-                <v-img
-                  :src="'/img/btn_google_signin_light_normal_web@2x.png'"
-                />
-              </div>
-            </v-col>
-          </v-row>
-        </v-form>
-      </div>
+  <div
+    class="d-flex justify-center align-center"
+    style="height: 80vh; background-color: #e0ece8"
+  >
+    <div class="d-flex flex-column justify-center align-center">
+      <v-img
+        style="width: 30vw; height: 30vw; border-radius: 50%"
+        src="https://picsum.photos/id/400/300/300"
+      />
+      <v-form class="row">
+        <v-row
+          class="d-flex flex-center flex justify-center align-center col-6 form-widget px-4 text-center"
+        >
+          <v-col>
+            <div
+              class="text-h5 text-teal-darken-4 font-weight-bold header pt-6"
+            >
+              Sign in
+            </div>
+            <div class="text-subtitle-3 pt-1 pb-3 text-teal">
+              Sign in with Social media account
+            </div>
+            <div
+              class="d-flex flex-row justify-center align-center logobtn py-2"
+              @click="login"
+            >
+              <v-img :src="'/img/btn_google_signin_light_normal_web@2x.png'" />
+            </div>
+          </v-col>
+        </v-row>
+      </v-form>
     </div>
   </div>
   <!-- <div v-else>
